8723f3c3bab2f2a2cf8ae9d27851b8cf83ccc81b
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-05-24 19:39:18 +0300

    add getters

--- src/Sokil/IsoCodes/Countries/Country.php
+++ src/Sokil/IsoCodes/Countries/Country.php
@@ -14,6 +14,26 @@ class Country extends \Sokil\IsoCodes\Database\Entry
     
     public $official_name;
     
+    public function getAlpha2()
+    {
+        return $this->alpha_2_code;
+    }
+    
+    public function getAlpha3()
+    {
+        return $this->alpha_2_code;
+    }
+    
+    public function getNumericCode()
+    {
+        return $this->numeric_code;
+    }
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+    
     public function getLocalName()
     {
         return dgettext($this->_database->getIso(), $this->name);
