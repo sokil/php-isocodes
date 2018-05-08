085545c36a88a996b4a574bc774fecd47b0ec17f
Dmytro Sokil <dmytro.sokil@gmail.com>
2018-05-08 22:15:32 +0300

    updat script

--- update_db.sh
+++ update_db.sh
@@ -2,7 +2,8 @@
 
 CURRENT_DIR=$(dirname $(readlink -f $0))
 PKG_ISOCODES_DIR=$CURRENT_DIR/build/iso-codes
-MESSAGES_PATH=$CURRENT_DIR/messages
+PKG_ISOCODES_REPO="https://salsa.debian.org/iso-codes-team/iso-codes.git"
+MESSAGES_DIR=$CURRENT_DIR/messages
 DATABASES_DIR=$CURRENT_DIR/databases
 
 # update pkg-isocodes source
@@ -12,16 +13,20 @@ if [[ -d $PKG_ISOCODES_DIR ]]; then
 else
     mkdir -p $PKG_ISOCODES_DIR
     cd $PKG_ISOCODES_DIR
-    git clone https://anonscm.debian.org/git/pkg-isocodes/iso-codes.git .
+    git clone $PKG_ISOCODES_REPO .
 fi
 
 cd $CURRENT_DIR
 
 # clear previous database and locales files
-rm -rf $MESSAGES_PATH
-mkdir -p $MESSAGES_PATH
+rm -rf $MESSAGES_DIR
+mkdir -p $MESSAGES_DIR
 rm -rf $DATABASES_DIR
 mkdir -p $DATABASES_DIR
+
+# add copyright notice
+echo "This files is part of iso-codes library.\nSee license agreement at ${PKG_ISOCODES_REPO}" > $DATABASES_DIR/LICENSE
+echo "This files is part of iso-codes library.\nSee license agreement at ${PKG_ISOCODES_REPO}" > $MESSAGES_DIR/LICENSE
 
 # move database files
 cp $PKG_ISOCODES_DIR/data/iso_*.json $DATABASES_DIR
@@ -34,15 +39,29 @@ for database_file in `ls -1 $DATABASES_DIR`; do
     for locale_file in `ls -1 $source_locale_dir | grep .po`; do
         locale_name=`echo $locale_file | sed "s/.po//g"`
         # copy locale file
-        target_locale_dir=$MESSAGES_PATH/$locale_name/LC_MESSAGES
+        target_locale_dir=$MESSAGES_DIR/$locale_name/LC_MESSAGES
         mkdir -p $target_locale_dir
         cp ${source_locale_dir}/${locale_file} ${target_locale_dir}/${gettext_domain}.po
         msgfmt ${target_locale_dir}/${gettext_domain}.po -o ${target_locale_dir}/${gettext_domain}.mo
     done;
 done
 
+# locate composer
+COMPOSER_PATH=`which composer`
+if [[ $? -eq 1 ]]; then
+    COMPOSER_PATH=`which composer.phar`
+    if [[ $? -eq 1 ]]; then
+        echo -e "\033[0;31m\n\nCan not locale composer for testing build.\033[0m\n\n"
+    fi
+fi
+
 # start tests
-composer.phar test
+$COMPOSER_PATH test
+
+# message on success test
+if [[ $? -eq 0 ]]; then
+    echo -e "\033[0;32m\n\nDatabase successfully updated. Verify difference and commit.\033[0m\n\n"
+fi
 
 # update version in README.md
 cd $PKG_ISOCODES_DIR
@@ -51,10 +70,7 @@ BUILD_DATE=`date "+%Y-%m-%d %H:%M"`
 cd $CURRENT_DIR
 sed -i -E "s/Database version: .*/Database version: ${VERSION} from ${BUILD_DATE}/" README.md
 
-# success
-if [[ $? -eq 0 ]]; then
-    echo -e "\033[0;32m\n\nDatabase successfully updated. Verify difference and commit.\033[0m\n\n"
-fi
+
 
 
 
