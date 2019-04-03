e8ee81a668fae519ec57b1eb8d0683e5d490f377
Henrique Moody <henriquemoody@gmail.com>
2019-04-03 15:26:20 +0200

    Enforce all files to declare strict types
    Signed-off-by: Henrique Moody <henriquemoody@gmail.com>

--- tests/Database/CurrenciesTest.php
+++ tests/Database/CurrenciesTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Sokil\IsoCodes\Databases;
 
 use Sokil\IsoCodes\Database\Currencies\Currency;
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 putenv('LANGUAGE=uk_UA.UTF-8');
 putenv('LC_ALL=uk_UA.UTF-8');
 setlocale(LC_ALL, 'uk_UA.UTF-8');
