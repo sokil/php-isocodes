5422e0491775c393f2e43fe17e3fa9ce5682d268
mayhem <mayhem42@protonmail.com>
2022-03-17 01:08:12 +0200

    remove unused

--- tests/Database/CountriesTest.php
+++ tests/Database/CountriesTest.php
@@ -7,7 +7,6 @@ namespace Sokil\IsoCodes\Database;
 use Sokil\IsoCodes\Database\Countries\Country;
 use Sokil\IsoCodes\IsoCodesFactory;
 use PHPUnit\Framework\TestCase;
-use Sokil\IsoCodes\TranslationDriver\SymfonyTranslationDriver;
 
 class CountriesTest extends TestCase
 {
